e7980c7d1e95820176cc61e3f03a6b59b2bf9dc6
AnuShiva14 <scs.annapoorani@gmail.com>
2024-03-04 11:30:59 +0900

    init commit

---
+++ src/extensions/appCustExtenWp/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAppCustExtenWpApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AppCustExtenWpApplicationCustomizerStrings' {
+  const strings: IAppCustExtenWpApplicationCustomizerStrings;
+  export = strings;
+}
